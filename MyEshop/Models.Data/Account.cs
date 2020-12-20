b3cc2d34a9a84dacae85eaeba8f691016ef39de3
Ayoub Chaabi <ayoubchaabi.contact@gmail.com>
2020-12-20 16:08:11 +0100

    [Smart-Eshop] Add project to github

---
+++ MyEshop/Models.Data/Account.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyEshop.Models.Data
+{
+    public class Account 
+    {
+        public  int Id { get; set; }
+
+        [EmailAddress]
+        public string Email { get; set; }
+
+        public  string Compte { get; set; }
+
+    }
+}
---
+++ MyEshop/Models.Data/Client.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyEshop.Models.Data
+{
+    public class Client
+    {
+        
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDay { get; set; }
+        public Account Account { get; set; }
+        
+    }
+
+
+}
